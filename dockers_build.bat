fc2f1a6051621e9c346deb892bd055cce0ea41df
OmerAvi96 <omeravi96@gmail.com>
2024-05-01 09:17:45 +0300

    Fixed rupee typo for docker image name

--- dockers_build.bat
+++ dockers_build.bat
@@ -5,7 +5,7 @@ docker build -f Dockerfiles/Dockerfile.py_euro_shekel -t py-euro-shekel .
 docker build -f Dockerfiles/Dockerfile.js_dollar_shekel -t js-dollar-shekel .
 docker build -f Dockerfiles/Dockerfile.js_euro_shekel -t js-euro-shekel .
 docker build -f Dockerfiles/Dockerfile.js_pound_shekel -t js-pound-shekel .
-docker build -f Dockerfiles/Dockerfile.js_ruppe_shekel -t js-ruppe-shekel .
+docker build -f Dockerfiles/Dockerfile.js_rupee_shekel -t js-rupee-shekel .
 docker build -f Dockerfiles/Dockerfile.js_wan_shekel -t js-wan-shekel .
 docker build -f Dockerfiles/Dockerfile.js_yen_shekel -t js-yen-shekel .
 docker build -f Dockerfiles/Dockerfile.js_handler -t js-handler .
